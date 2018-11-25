b075c593d71a9dcf9826cbca7d72fba0e92a6136
Egor <33986910+EgorVolokitin@users.noreply.github.com>
2018-11-26 02:08:37 +0700

    Ev (#18)
    * Что сделано:
    1) Фикс проблем с отображением страниц
    2) Фикс ошибок мерджа
    
    Что с package.json:
    1) Исправлены проблемы с зависимостями
    
    * Что сделано:
    1) сделан конфиг для работы с бд
    
    * Что сделано:
    1) Настроены миграции
    
    * Что сделано:
    1) Создание таблицы пользователей
    
    Что с package.json:
    1) Добавлены новые dev-зависимости
    
    * Что сделано:
    1) Добавлены миграции для таблицы пользователей
    2) Добавлен ORM Sequelize для работы с базой
    3) Сделаны изменения в конфиге
    4) Добавлена регистрация и аутентификация пользователей
    5) Добавлены имена к инпутам в signin.html
    
    Что с package.json:
    1) Добавлены новые зависимости
    
    * Логин + аутентификация
    
    * work
    
    * Что сделано:
    1) Сделана смена хэдера в зависимости от аутентифицированности пользователя
    2) Создана отдельная функция рендера страницы в зависимости от аутентификации в соответствии с DRY
    3) Добавлено несколько TODO
    
    * work
    
    * Что сделано:
    1) убрана перезагрузка страницы при нажатии на выпадающее меню nickname
    
    * выкидываем пользователя в случае если токен аутентификации не верный.
    
    * Что сделано:
    1) Обновлена функция валидации токена
    2) Функция рендера страницы заменена на функцию получения объекта свойств рендера
    3) Страница профиля теперь отображается по переданному в запросе id
    4) В хэдэре спрятаны маленькие иконки  и заменена ссылка профиля на ссылку с id
    5) В profile.html ник теперь приходит с сервера
    
    Что с package.json:
    1) Есть новые зависимости

--- api/auth/validateToken.js
+++ api/auth/validateToken.js
@@ -4,6 +4,11 @@ const config = require('config');
 const salt = config.get('hashes.jwtSecretKey');
 
 module.exports = async (token) => {
+  if(!token) {
+    console.error('Отсутствует токен аутентификации');
+    return await null;
+  }
+
   try {
     return await jwt.verify(token, salt);
   } catch (error) {
